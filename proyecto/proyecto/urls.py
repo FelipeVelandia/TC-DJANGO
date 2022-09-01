456b69350c302a11ec7880b688f12463958e7467
VelanD <juanfelipevn@gmail.com>
2022-09-01 07:29:40 -0500

    clase 3-2

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('libros.urls'))
+    path('',include('libros.urls')),
+    path('autor/',include('autores.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
